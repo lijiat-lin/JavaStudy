e95f1256a380106e6462f3b664d23b07d1569d19
lijiat-lin <lijiat_lin@163.com>
2020-11-16 18:23:45 +0800

    阻塞队列

--- src/main/java/com/example/knowledge/test/Test.java
+++ src/main/java/com/example/knowledge/test/Test.java
@@ -1,50 +1,11 @@
 package com.example.knowledge.test;
 
 
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.TimeUnit;
 
 public class Test {
 
-
     public static void main(String[] args) throws InterruptedException {
-        //创建长度为3的阻塞队列
 
-        //默认非公平锁队列
-        BlockingQueue<String> blockingQueue = new SynchronousQueue<>();
-        new Thread(() -> {
-            try {
-                System.out.println(Thread.currentThread().getName()+"\t put 1");
-                blockingQueue.put("1");
-
-                System.out.println(Thread.currentThread().getName()+"\t put 2");
-                blockingQueue.put("2");
-
-                System.out.println(Thread.currentThread().getName()+"\t put 3");
-                blockingQueue.put("3");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        },"AAA").start();
-
-        new Thread(() -> {
-            try {
-                Thread.sleep(3000);
-                System.out.println(Thread.currentThread().getName()+"\t"+blockingQueue.take());
-
-                Thread.sleep(3000);
-                System.out.println(Thread.currentThread().getName()+"\t"+blockingQueue.take());
-
-                Thread.sleep(3000);
-                System.out.println(Thread.currentThread().getName()+"\t"+blockingQueue.take());
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        },"BBB").start();
 
     }
 
