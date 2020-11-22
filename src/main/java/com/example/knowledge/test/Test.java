c69dd3972d7dfb82f8f56c1519434eab6f4ad514
lijiat-lin <lijiat_lin@163.com>
2020-11-22 18:55:18 +0800

    CAS

--- src/main/java/com/example/knowledge/test/Test.java
+++ src/main/java/com/example/knowledge/test/Test.java
@@ -1,10 +1,18 @@
 package com.example.knowledge.test;
 
-
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+
+        synchronized (new Object()){
+
+        }
+
+        new ReentrantLock().lock();
+
+
 
 
     }
