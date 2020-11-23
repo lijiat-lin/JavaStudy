6a8156cfd2d4447129a824f8ffbf6b84535bc4ae
lijiat-lin <lijiat_lin@163.com>
2020-11-23 18:52:03 +0800

    阻塞队列

--- src/main/java/com/example/knowledge/test/Test.java
+++ src/main/java/com/example/knowledge/test/Test.java
@@ -1,17 +1,34 @@
 package com.example.knowledge.test;
 
-import java.util.concurrent.locks.ReentrantLock;
 
-public class Test {
 
-    public static void main(String[] args) {
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
-        synchronized (new Object()){
+class ShareData{
+    private volatile boolean FLAG = true;
+    private AtomicInteger atomicInteger = new AtomicInteger();
+
+    BlockingQueue<String> blockingQueue = null;
+
+    public ShareData(BlockingQueue<String> blockingQueue){
+        this.blockingQueue = blockingQueue;
+        System.out.println(blockingQueue.getClass().getName());
+    }
+
+    public void prod() throws Exception{
+        while (FLAG){
 
         }
 
-        new ReentrantLock().lock();
+    }
 
+}
+public class Test {
+
+    public static void main(String[] args) {
+        ShareData shareData = new ShareData(new ArrayBlockingQueue<>(10));
 
 
 
