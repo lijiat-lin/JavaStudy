8e61baa2619c5efae1cbdb4d0db2e81c3e698299
lijiat-lin <lijiat_lin@163.com>
2020-12-04 18:50:33 +0800

    Linux命令

--- src/main/java/com/example/knowledge/test/Test.java
+++ src/main/java/com/example/knowledge/test/Test.java
@@ -1,12 +1,17 @@
 package com.example.knowledge.test;
 
+import net.minidev.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Test {
 
     public static void main(String[] args)  {
-        System.out.println("Test");
-        System.out.println("Test");
+
+        Map<String,String> map = new HashMap<>();
+        map.put("amount","1.23");
+        System.out.println(JSONObject.toJSONString(map));
     }
-
-
-
 }
